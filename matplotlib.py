6fb132b63b7783d6f523068bad3792d17a619456
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2021-11-10 14:04:58 +0530

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -246,6 +246,241 @@ plt.show()
 #----------
 
 
+#Linestyle
+#You can use the keyword argument linestyle, or shorter ls, to change the style of the plotted line:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+#---------------------------------------
+#plt.plot(ypoints, linestyle = 'dashed')
+#The line style can be written in a shorter syntax:
+
+#linestyle can be written as ls.
+
+#dotted can be written as :.
+
+#dashed can be written as --.
+#'solid' (default)	'-'	
+
+#'dashdot'	'-.'	
+#'None'	'' or ' '
+#-------------------------------
+#Line Color
+#You can use the keyword argument color or the shorter c to set the color of the line:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, color = 'r')
+plt.show()
+#-------------------------
+
+#plt.plot(ypoints, c = 'hotpink')
+#------------------------------
+#Line Width
+#You can use the keyword argument linewidth or the shorter lw to change the width of the line.
+
+#The value is a floating number, in points:
 
 
+#Shorter syntax:
+
+#plt.plot(ypoints, ls = ':')
+
+#--------------------------
+
+#Multiple Lines
+#You can plot as many lines as you like by simply adding more plt.plot() functions:
+  
+# Draw two lines by specifying a plt.plot() function for each line:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
+
+#-------------same as below---
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1, x2, y2)
+plt.show()
+#-----------------------------------
+
+#Create Labels for a Plot
+#With Pyplot, you can use the xlabel() and ylabel() functions to set a label for the x- and y-axis.
+
+Add labels to the x- and y-axis:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+#-------------------------------
+#Create a Title for a Plot
+#With Pyplot, you can use the title() function to set a title for the plot.
+
+#Example
+#Add a plot title and labels for the x- and y-axis:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+#-----------------------------------------
+
+#Set Font Properties for Title and Labels
+#You can use the fontdict parameter in xlabel(), ylabel(), and title() to set font properties for the title and labels.
+#Example
+#Set font properties for the title and labels:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+
+plt.title("Sports Watch Data", fontdict = font1)
+plt.xlabel("Average Pulse", fontdict = font2)
+plt.ylabel("Calorie Burnage", fontdict = font2)
+
+plt.plot(x, y)
+plt.show()
+#-----------------------------------------
+  
+#Position the Title
+#You can use the loc parameter in title() to position the title.
+
+#Legal values are: 'left', 'right', and 'center'. Default value is 'center'.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.show()
+#------------------------
+
+#Add Grid Lines to a Plot
+#With Pyplot, you can use the grid() function to add grid lines to the plot.
+
+#Example
+#Add grid lines to the plot:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid()
+
+plt.show()
+#--------------------------------
+#Specify Which Grid Lines to Display
+#You can use the axis parameter in the grid() function to specify which grid lines to display.
+
+#Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
+
+Example
+Display only grid lines for the x-axis:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'x')
+
+plt.show()
+#----------------------------------
+#Display only grid lines for the y-axis:
+#plt.grid(axis = 'y')
+#-------------------------------
+#Set Line Properties for the Grid
+#You can also set the line properties of the grid, like this: grid(color = 'color', linestyle = 'linestyle', linewidth = number).
+  
+  Example
+Set the line properties of the grid:
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+
+plt.show()
+
+#-----------------------------------------------
+#Matplotlib Subplots
+
+
+
+
+  
   
